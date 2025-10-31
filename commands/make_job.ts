63e9cea9cd37d1cd3c83fe366710119f6e605223
Onix Black <onix@address.black>
2025-11-01 02:51:59 +0700

    feat: make:job command now uses jobsDirectory config

--- commands/make_job.ts
+++ commands/make_job.ts
@@ -1,6 +1,7 @@
 import { BaseCommand, args } from '@adonisjs/core/ace'
 import { stubsRoot } from '../stubs/index.js'
 import type { CommandOptions } from '@adonisjs/core/types/ace'
+import type { BullMQConfig } from '../src/types.js'
 
 export default class MakeJob extends BaseCommand {
   public static commandName = 'make:job'
@@ -12,8 +13,14 @@ export default class MakeJob extends BaseCommand {
 
   async run() {
     const codemods = await this.createCodemods()
+    
+    // Read jobsDirectory from config
+    const config = this.app.config.get<BullMQConfig>('bullmq')
+    const jobsDirectory = config?.jobsDirectory || 'app/jobs'
+    
     await codemods.makeUsingStub(stubsRoot, 'job/main.stub', {
       entity: this.app.generators.createEntity(this.name),
+      jobsDirectory,
     })
   }
 }
